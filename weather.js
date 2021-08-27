b38db012fb9975902ad00cef64b875d82a6d600f
Md Jahir Uddin Munna <mdjahiruddinmunna@gmail.com>
2021-08-28 05:20:54 +0600

    first commit

---
+++ weather.js
@@ -0,0 +1,7 @@
+// api = 'api.openweathermap.org/data/2.5/weather?q=bogra&units=metric&appid=a771a88d4f683951b388a56c2e844adc'
+
+// icon = http://openweathermap.org/img/wn/10n@2x.png
+
+document.getElementById('search-btn').onclick = () => {
+
+}
